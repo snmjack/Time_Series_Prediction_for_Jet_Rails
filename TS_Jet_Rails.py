0e45e7b417f0b7e249aef473f83be7721f2e6b12
Swapnil Mahajan <31958717+snmjack@users.noreply.github.com>
2018-07-27 17:05:56 +0530

    Update TS_Jet_Rails.py

--- TS_Jet_Rails.py
+++ TS_Jet_Rails.py
@@ -50,6 +50,76 @@ for i in (train, test, test_original, train_original):
     i['year']=i.Datetime.dt.year 
     i['month']=i.Datetime.dt.month 
     i['day']=i.Datetime.dt.day
-    
-    
     i['Hour']=i.Datetime.dt.hour 
+    
+# Adding additional feature of Weekdaya and Weekend. 
+train['day of week']=train['Datetime'].dt.dayofweek
+temp = train['Datetime']
+
+# No. 5 & 6 will be weekend, so replace it with 1 
+def applyer(row):
+    if row.dayofweek == 5 or row.dayofweek == 6:
+        return 1
+    else:
+        return 0
+
+# Apply the values of Weekdays as 0 and Weekend as 1 
+temp2 = train['Datetime'].apply(applyer)
+train['weekend']=temp2
+
+
+# ---------------------------  EXPLORATOORY DATA ANALYSIS (EDA)  -------------------------------------- #
+
+# Plotting of Count of passenger on time axis
+train.index = train['Datetime'] # indexing the Datetime to get the time period on the x-axis.
+df=train.drop('ID',1)           # drop ID variable to get only the Datetime on x-axis.
+ts = df['Count']
+plt.figure(figsize=(16,8))
+plt.plot(ts, label='Passenger Count')
+plt.title('Time Series')
+plt.xlabel("Time(year-month)")
+plt.ylabel("Passenger count")
+plt.legend(loc='best')
+
+# Year wise customer count plot
+train.groupby('year')['Count'].mean().plot.bar()
+
+# Month wise customer count plot
+train.groupby('month')['Count'].mean().plot.bar()
+
+# Monthly mean of each year seperately
+temp=train.groupby(['year', 'month'])['Count'].mean()
+temp.plot(figsize=(15,5), title= 'Passenger Count(Monthwise)', fontsize=14)
+
+# Day Wise customer count plot
+train.groupby('day')['Count'].mean().plot.bar()
+
+# Hour wise customer count plot
+train.groupby('Hour')['Count'].mean().plot.bar()
+
+# Weekend and Weekday Plot
+train.groupby('weekend')['Count'].mean().plot.bar()
+
+# Every day of the week customer count plot
+train.groupby('day of week')['Count'].mean().plot.bar()
+
+# ---------------------------  AGGREGATE HOURLY DATA TO DAILY, WEEKLY AND MONTHLY  -------------------------------------- #
+train=train.drop('ID',1)
+train.Timestamp = pd.to_datetime(train.Datetime,format='%d-%m-%Y %H:%M') 
+train.index = train.Timestamp
+
+hourly = train.resample('H').mean()  # Hourly time series
+daily = train.resample('D').mean()   # Converting to daily mean
+weekly = train.resample('W').mean()  # Converting to weekly mean
+monthly = train.resample('M').mean() # Converting to monthly mean
+
+
+# Plot Hourly, Daily, Weekly and Monthly data
+fig, axs = plt.subplots(4,1)
+hourly.Count.plot(figsize=(15,8), title= 'Hourly', fontsize=14, ax=axs[0])
+daily.Count.plot(figsize=(15,8), title= 'Daily', fontsize=14, ax=axs[1])
+weekly.Count.plot(figsize=(15,8), title= 'Weekly', fontsize=14, ax=axs[2])
+monthly.Count.plot(figsize=(15,8), title= 'Monthly', fontsize=14, ax=axs[3])
+plt.show()
+
+
