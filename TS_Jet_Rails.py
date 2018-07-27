c1e7bc1beea2c9dd2d7b7d69913e71c5f8021bf1
Swapnil Mahajan <31958717+snmjack@users.noreply.github.com>
2018-07-27 15:14:55 +0530

    Create TS_Jet_Rails.py

---
+++ TS_Jet_Rails.py
@@ -0,0 +1,15 @@
+# Import Mathematical Libraries
+import pandas as pd         # For DataFrames          
+import numpy as np          # For mathematical calculations
+from pandas import Series        # To work on series
+
+# Import Plotting Libraries
+import matplotlib.pyplot as plt  # For plotting graphs
+%matplotlib inline
+
+# Import Warning Filter Libraries
+import warnings                   # To ignore the warnings
+warnings.filterwarnings("ignore")
+
+# Import DateTime Libraries
+from datetime import datetime    # To access datetime
